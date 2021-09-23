e74844f16145d42c01d7d5cefd6444ff105c0dca
Sakil Jomadder <sakilorb@icloud.com>
2021-09-23 23:23:01 +0600

    git initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+const MainApp = styled.div`
+  background-color: red;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <MainApp>
+        <h1>Hello styled</h1>
+      </MainApp>
+    </>
   );
 }
 
